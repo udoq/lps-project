5f583777716945c142967243faa5406b87b270aa
Udo Quattelbaum <78760654+uqgh@users.noreply.github.com>
2021-12-12 22:16:22 +0100

    MVP, Routing und ein bisschen nice

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { LearningComponent } from './learning/learning.component';
 import { QuestionComponent } from './question/question.component';
 import { SummaryComponent } from './summary/summary.component';
+import { TestingComponent } from './testing/testing.component';
+import { TryingComponent } from './trying/trying.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 const routes: Routes = [
   {path:'', redirectTo: 'welcome', pathMatch: "full"},
   {path: 'welcome', component: WelcomeComponent},
   {path: 'learning', component: LearningComponent},
+  {path: 'trying', component: TryingComponent},
+  {path: 'testing', component: TestingComponent}
 ];
 
 @NgModule({
